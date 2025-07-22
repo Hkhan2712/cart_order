b3fd9ab00a1628a94fd02810f26528a46377a970
Hkhan2712 <nguynkit3107@gmail.com>
2025-07-22 15:13:57 +0700

    Removed: Lib login form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Register</title>
+    <title>AquaTerra - Register</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/animate/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/animsition/css/animsition.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/select2/select2.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/vendor/daterangepicker/daterangepicker.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/css/util.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('auth-assets/css/main.css') }}">
 </head>
@@ -68,15 +62,7 @@
     </div>
 
     <div id="dropDownSelect1"></div>
-
-    <script src="{{ asset('auth-assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/animsition/js/animsition.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/bootstrap/js/popper.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/select2/select2.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/daterangepicker/moment.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
-    <script src="{{ asset('auth-assets/vendor/countdowntime/countdowntime.js') }}"></script>
+    
     <script src="{{ asset('auth-assets/js/main.js') }}"></script>
 
 </body>
